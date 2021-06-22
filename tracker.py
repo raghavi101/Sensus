5ee62ffa943d9d60cdbc919db643b2e58487beef
raghavi101 <raghaviss11@gmail.com>
2021-06-22 09:22:38 +0530

    py correction

--- tracker.py
+++ tracker.py
@@ -6,6 +6,6 @@ page = requests.get(
     "https://www.moneycontrol.com/technical-analysis/indian-indices/sensex-4/daily")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-first_h1 = soup.select('strong')[35].text
+first_h1 = soup.select('strong')[31].text
 print(first_h1)
 
