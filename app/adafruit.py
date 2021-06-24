e33f43373f7a84ca7e04841caa95ae7c681324ff
raghavi101 <raghaviss11@gmail.com>
2021-06-24 23:28:09 +0530

    env var initialized

--- app/adafruit.py
+++ app/adafruit.py
@@ -1,14 +1,17 @@
 from Adafruit_IO.client import Client, Feed, Data
+import os
 import tracker
 
-ADAFRUIT_IO_KEY = 'aio_xSUD52dazxoXwfbiBTQ8bQNpoTGl'
+ADAFRUIT_IO_KEY = os.environ.get('IO_KEY')
 
-ADAFRUIT_IO_USERNAME = 'sraghavi'
+ADAFRUIT_IO_USERNAME = os.environ.get('IO_USERNAME')
 
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
-
 aio.send('gaugemeter', value=tracker.callval())
+
+
+
 
  
 
--- app/tracker.py
+++ app/tracker.py
@@ -5,7 +5,7 @@ def callval():
     page = requests.get(
     "https://www.moneycontrol.com/technical-analysis/indian-indices/sensex-4/daily")
     soup = BeautifulSoup(page.content, 'html.parser')
-    rsival = soup.select('strong')[35].text
+    rsival = soup.select('strong')[31].text
     return rsival 
 
 
